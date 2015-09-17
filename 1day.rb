1cd1caff3a3caee6d4bca512c49be87d854bccfc
ChoDonghyun <chodonghyun@ChoDonghyunui-MacBook-Air.local>
2015-09-18 00:23:11 +0900

    1 day practice

---
+++ 1day.rb
@@ -0,0 +1,20 @@
+puts "print 'iello, world'"
+puts 'hello, world'
+
+puts "\"hello, Ruby\" find \"Ruby\" index"
+
+puts "print myname for 10 times"
+x = 0
+while x < 10
+  puts 'dh cho'
+  x = x + 1
+end
+
+puts " 'This is sentence number 1' for 1 ~ 10"
+
+x = 1
+until x == 10
+  puts "This is sentence number #{x}" 
+  x = x + 1
+end
+
